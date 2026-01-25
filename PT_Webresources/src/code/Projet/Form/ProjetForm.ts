76113528f4ccd5b8a7837a048b35fd0566265a05
Benji <benjaminimare.professionnel@gmail.com>
2026-01-25 08:56:18 -0500

    webresource-setup

---
+++ PT_Webresources/src/code/Projet/Form/ProjetForm.ts
@@ -0,0 +1,5 @@
+export class ProjetForm {
+    static async onLoad(executionContext: Xrm.Events.EventContext): Promise<void> {
+        const formContext = executionContext.getFormContext();
+    }
+}
---
+++ PT_Webresources/webpack.common.js
@@ -0,0 +1,37 @@
+const ESLintPlugin = require("eslint-webpack-plugin");
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+  devtool: "source-map",
+  entry: {
+    ProjetForm: "./src/code/Projet/Form/ProjetForm.ts",
+  },
+  output: {
+    filename: "[name].js",
+    sourceMapFilename: "maps/[name].js.map",
+    path: path.resolve(__dirname, "./src/Webresources/js"),
+    library: ["pt", "[name]"],
+    libraryTarget: "var",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ESLintPlugin({
+      fix: true,
+      extensions: ["ts", "tsx"],
+      lintDirtyModulesOnly: true,
+      failOnError: true,
+    }),
+  ],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
